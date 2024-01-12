597aba849812c85493d187704a83039c87c669ac
Kazi Anwar <kazisameen2702@gmail.com>
2024-01-11 22:38:04 -0500

    completed leetcode problem 100 same tree

--- problemsPy/100.SameTree.py
+++ problemsPy/100.SameTree.py
@@ -16,3 +16,24 @@
 # Output: false
 
 # Solution:
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    from typing import Optional
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q: #p means exists, not p means dne, if both p and q dne, theyre equal so return True
+            return True
+        
+        if not p or not q or p.val != q.val: #if p OR q dne like one exists but the other does not, return false. If a val doesnt match the other one, return false
+            return False
+
+        
+        x = self.isSameTree(p.right, q.right) #cheks if the right subtrees match
+        y = self.isSameTree(p.left, q.left) #same as above
+        if x and y == True: #if x and y are true, theyre the same tree
+            return True
+        return False  
